5ed97fe20ac57b02bdd2565b375f5d23fc8387ac
nirupamrocks <166874184+nirupamrocks@users.noreply.github.com>
2024-04-15 00:28:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use("/api/v1/admin", require("./routes/adminRoutes"));
 
 //STATIC ROUTES 
 //app.get("*",function(req,res)  {
-    res.sendFile(path.join(__dirname,"./client/build/index.html"));
+   // res.sendFile(path.join(__dirname,"./client/build/index.html"));
     
 });
 //port
